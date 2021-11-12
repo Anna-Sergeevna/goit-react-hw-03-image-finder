210dcd6f911736d7988b5f0b83c66660aa25b19c
Anna-Sergeevna <4042808@gmail.com>
2021-11-12 17:07:57 +0200

    Добавляет зависимости

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Container from 'components/Container';
+import Section from 'components/Section';
+import './App.css';
+
+export default function App() {
+  return (
+    <Container title="">
+      <Section title=""></Section>
+    </Container>
+  );
+}
